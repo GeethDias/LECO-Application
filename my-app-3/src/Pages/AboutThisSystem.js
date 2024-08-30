e49876e2542e5016831e58c1ddb24416314896cf
GeethDias <Udageeth20@outlook.com>
2024-08-30 23:56:45 +0530

    Added Footer for the AboutThisSystem.js File

--- my-app-3/src/Pages/AboutThisSystem.js
+++ my-app-3/src/Pages/AboutThisSystem.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import MyNavbar from '../Components/Navbar'
+import Footer from '../Components/Footer'
 
 const AboutThisSystem = () => {
     return (
         <div>
             <MyNavbar/>
             <h1>Acceptable Use Policy</h1>
+            <Footer/>
         </div>
     )
 }
